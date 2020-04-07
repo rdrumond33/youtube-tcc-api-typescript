6f7dced4ab59600c9e5085cfd7b0449deb256b05
Rodrigo Drumond de Paula <rodrigodru33@gmail.com>
2020-04-07 13:23:06 -0300

    improve controller

--- src/controllers/ChannelsController.ts
+++ src/controllers/ChannelsController.ts
@@ -1,30 +1,43 @@
-import { Request, Response } from 'express'
-import Channel from '../schemas/Channel'
-import { getIds, normalizeData } from '../setting/youtube'
+import { Request, Response } from "express";
+import Channel from "../schemas/Channel";
+import { getIds, normalizeData } from "../setting/youtube";
 class ChannelController {
   public async index(req: Request, res: Response): Promise<Response> {
-    const Channels = await Channel.find()
-    return res.json(Channels)
+    const Channels = await Channel.find();
+    return res.json(Channels);
   }
 
   public async ping(req: Request, res: Response): Promise<Response> {
-    return res.json("Pong")
+    return res.json("Pong");
   }
 
   public async create(req: Request, res: Response): Promise<Response> {
-    const country = req.params.country.toUpperCase()
+    const country = req.params.country.toUpperCase();
 
-    const data = await getIds(country)
-    const dataNormalize = await normalizeData(data, country)
-    return res.json(dataNormalize)
+    const data = await getIds(country);
+    const dataNormalize = await normalizeData(data, country);
+    return res.json(dataNormalize);
   }
   public async show(req: Request, res: Response): Promise<Response> {
-    const country = req.params.country.toUpperCase()
-    
-    const Channels = await Channel.find({ country: country })
+    const country = req.params.country.toUpperCase();
 
-    return res.json(Channels)
+    const Channels = await Channel.find({ country: country });
+
+    return res.json(Channels);
+  }
+
+  public async count(req: Request, res: Response): Promise<Response> {
+    const count = await Channel.aggregate([
+      {
+        $group: {
+          _id: "$country",
+          count: { $sum: 1 },
+        },
+      },
+    ]);
+
+    return res.json({ count })
   }
 }
 
-export default new ChannelController()
+export default new ChannelController();
