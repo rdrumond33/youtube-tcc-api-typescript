a0890de59dd56ea45b870fe1e01062cc11f42d7c
Rodrigo Drumond de Paula <rodrigodru33@gmail.com>
2020-04-07 12:51:11 -0300

    add new route

--- src/controllers/ChannelsController.ts
+++ src/controllers/ChannelsController.ts
@@ -2,21 +2,28 @@ import { Request, Response } from 'express'
 import Channel from '../schemas/Channel'
 import { getIds, normalizeData } from '../setting/youtube'
 class ChannelController {
-  public async index (req: Request, res: Response): Promise<Response> {
+  public async index(req: Request, res: Response): Promise<Response> {
     const Channels = await Channel.find()
     return res.json(Channels)
   }
 
-  public async ping (req: Request, res: Response): Promise<Response> {
-    return res.json('Pong')
+  public async ping(req: Request, res: Response): Promise<Response> {
+    return res.json("Pong")
   }
 
-  public async create (req: Request, res: Response): Promise<Response> {
-    const country = req.params.country
+  public async create(req: Request, res: Response): Promise<Response> {
+    const country = req.params.country.toUpperCase()
 
     const data = await getIds(country)
     const dataNormalize = await normalizeData(data, country)
     return res.json(dataNormalize)
+  }
+  public async show(req: Request, res: Response): Promise<Response> {
+    const country = req.params.country.toUpperCase()
+    
+    const Channels = await Channel.find({ country: country })
+
+    return res.json(Channels)
   }
 }
 
--- src/routes.ts
+++ src/routes.ts
@@ -5,5 +5,5 @@ const routes = Router()
 routes.get('/tcc/ping', ChannelsController.ping)
 routes.get('/tcc/channels', ChannelsController.index)
 routes.get('/tcc/channels/:country', ChannelsController.create)
-
+routes.get('/tcc/channels/search/:country', ChannelsController.show)
 export default routes
