2ee777823d65153a940cca233a5bd2e36cd9b48c
Rodrigo Drumond de Paula <rodrigodru33@gmail.com>
2020-06-07 00:54:33 -0300

    fix-2

--- src/controllers/ChannelsController.ts
+++ src/controllers/ChannelsController.ts
@@ -1,141 +1,143 @@
-import { Request, Response } from "express";
-import Channel from "../schemas/Channel";
-import getIds from "../setting/youtube";
-import moment from "moment-timezone";
+import { Request, Response } from 'express'
+import Channel from '../schemas/Channel'
+import getIds from '../setting/youtube'
+import moment from 'moment-timezone'
 class ChannelController {
-  public async index(req: Request, res: Response): Promise<Response> {
-    const Channels = await Channel.find();
-    return res.json(Channels);
+  public async index (req: Request, res: Response): Promise<Response> {
+    const Channels = await Channel.find()
+    return res.json(Channels)
   }
 
-  public async ping(req: Request, res: Response): Promise<Response> {
-    return res.json("Pong");
+  public async ping (req: Request, res: Response): Promise<Response> {
+    return res.json('Pong')
   }
 
-  public async create(req: Request, res: Response): Promise<Response> {
-    const country = req.params.country.toUpperCase();
+  public async create (req: Request, res: Response): Promise<Response> {
+    const country = req.params.country.toUpperCase()
 
-    const data = await getIds(country);
-    return res.json(data);
+    const data = await getIds(country)
+    return res.json(data)
   }
 
-  public async show(req: Request, res: Response): Promise<Response> {
-    const country = req.params.country.toUpperCase();
+  public async show (req: Request, res: Response): Promise<Response> {
+    const country = req.params.country.toUpperCase()
 
-    const Channels = await Channel.find({ country: country });
+    const Channels = await Channel.find({ country: country })
 
-    return res.json(Channels);
+    return res.json(Channels)
   }
 
-  public async count(req: Request, res: Response): Promise<Response> {
+  public async count (req: Request, res: Response): Promise<Response> {
     const count = await Channel.aggregate([
       {
         $group: {
-          _id: "$country",
-          count: { $sum: 1 },
-        },
-      },
-    ]);
+          _id: '$country',
+          count: { $sum: 1 }
+        }
+      }
+    ])
 
-    return res.json({ count });
+    return res.json({ count })
   }
 
-  public async orderDay(req: Request, res: Response): Promise<Response> {
+  public async orderDay (req: Request, res: Response): Promise<Response> {
     const result = await Channel.find({
       createdAt: {
         $lt: new Date(),
-        $gte: new Date(new Date().setDate(new Date().getDate() - 1)),
-      },
-    });
+        $gte: new Date(new Date().setDate(new Date().getDate() - 1))
+      }
+    })
     if (result.length === 0) {
-      return res.json({ result: "Nao tem dado para esse dia" });
+      return res.json({ result: 'Nao tem dado para esse dia' })
     }
 
     const resultOrder = result.sort((a, b): number => {
-      if (parseInt(a.viewCount) < parseInt(b.viewCount)) return 1;
-      if (parseInt(b.viewCount) < parseInt(a.viewCount)) return -1;
-      return 0;
-    });
+      if (parseInt(a.viewCount) < parseInt(b.viewCount)) return 1
+      if (parseInt(b.viewCount) < parseInt(a.viewCount)) return -1
+      return 0
+    })
 
-    return res.json({ resultOrder });
+    return res.json({ resultOrder })
   }
 
-  public async orderDayPerContry(
+  public async orderDayPerContry (
     req: Request,
     res: Response
   ): Promise<Response> {
-    const country = req.params.country.toUpperCase();
+    const country = req.params.country.toUpperCase()
 
     const result = await Channel.find({
       country: country,
       createdAt: {
         $lt: new Date(),
-        $gte: new Date(new Date().setDate(new Date().getDate() - 1)),
-      },
-    });
+        $gte: new Date(new Date().setDate(new Date().getDate() - 1))
+      }
+    })
 
     if (result.length === 0) {
-      return res.json({ result: "Nao tem dado para esse dia" });
+      return res.json({ result: 'Nao tem dado para esse dia' })
     }
 
     const resultOrder = result.sort((a, b): number => {
-      if (parseInt(a.viewCount) < parseInt(b.viewCount)) return 1;
-      if (parseInt(b.viewCount) < parseInt(a.viewCount)) return -1;
-      return 0;
-    });
+      if (parseInt(a.viewCount) < parseInt(b.viewCount)) return 1
+      if (parseInt(b.viewCount) < parseInt(a.viewCount)) return -1
+      return 0
+    })
 
-    return res.json({ resultOrder });
+    return res.json({ resultOrder })
   }
-  public async videId(req: Request, res: Response): Promise<Response> {
-    const id = req.params.id;
 
-    const result = await Channel.find({ idVideo: id });
+  public async videId (req: Request, res: Response): Promise<Response> {
+    const id = req.params.id
 
-    return res.json({ result });
+    const result = await Channel.find({ idVideo: id })
+
+    return res.json({ result })
   }
-  public async datePerContry(req: Request, res: Response): Promise<Response> {
-    const date = moment.tz(`${req.params.date} 00:00`, "America/Sao_Paulo");
+
+  public async datePerContry (req: Request, res: Response): Promise<Response> {
+    const date = moment.tz(`${req.params.date} 00:00`, 'America/Sao_Paulo')
 
     const result = await Channel.aggregate([
       {
         $addFields: {
-          viewCountInt: { $toInt: "$viewCount" },
-        },
+          viewCountInt: { $toInt: '$viewCount' }
+        }
       },
       {
         $match: {
           createdAt: {
             $gte: new Date(date.utc().format()),
-            $lt: new Date(date.add(1, "days").utc().format()),
-          },
-        },
-      },
-      {
-        $limit: 10,
-      },
-      {
-        $sort: { viewCountInt: -1 },
+            $lt: new Date(date.add(1, 'days').utc().format())
+          }
+        }
       },
       {
         $group: {
-          _id: "$country",
+          _id: '$country',
           videos: {
             $push: {
-              idVideo: "$idVideo",
-              country: "$country",
-              rank: "$rank",
-              viewCount: "$viewCount",
-              likeCount: "$likeCount",
-              dislikeCount: "$dislikeCount",
-              channel: "$channel",
-              title: "$title",
-            },
-          },
-        },
+              idVideo: '$idVideo',
+              country: '$country',
+              rank: '$rank',
+              viewCount: '$viewCount',
+              likeCount: '$likeCount',
+              dislikeCount: '$dislikeCount',
+              channel: '$channel',
+              title: '$title'
+            }
+          }
+        }
       },
-    ]);
+      {
+        $limit: 10
+      },
+      {
+        $sort: { viewCountInt: -1 }
+      }
+    ])
 
-    return res.json({ result });
+    return res.json({ result })
   }
 }
-export default new ChannelController();
+export default new ChannelController()
